ecbc3794768e996570a4514d6da55493b058225b
sanjay-7092 <sajusanjay7092@gmail.com>
2023-07-02 00:32:22 -0700

    SC:just name changing

--- nospace.java
+++ nospace.java
@@ -10,10 +10,10 @@ class nospace
  int n=Integer.parseInt(d.readLine());
  int a[]=new int[m],t;
  int b[]=new int[n];
- System.out.println("___");
+ System.out.println("__First Array__");
  for(int i=0;i<m;i++)
   a[i]=Integer.parseInt(d.readLine());
- System.out.println("___");
+ System.out.println("__Second Array__");
  for(int i=0;i<n;i++)
   b[i]=Integer.parseInt(d.readLine());
  for(int i=0;i<m;i++)
@@ -26,9 +26,10 @@ class nospace
      }
  Arrays.sort(b);
   System.out.println("__ANSWER__");
+System.out.println("__First Array_");
  for(int k :a)
   System.out.println(k);
-  System.out.println("___");
+  System.out.println("__Second Array_");
  for(int k :b)
   System.out.println(k);
 }}
